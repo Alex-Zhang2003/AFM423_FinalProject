8bb379917cf6b775b46e3fabd8f34f7e2ed05563
Alex Zhang <a282zhan@uwaterloo.ca>
2025-04-15 19:27:50 -0400

    fixed loader

--- loader/fi_loader.py
+++ loader/fi_loader.py
@@ -5,22 +5,16 @@ import torch
 
 
 def get_raw_dataset(data_dir, method, cf, train):
-    """
-    There are three methods of normalization: z-score, min-max, and decimal-precision
-    """
     if train:
         path = os.path.join(data_dir, method, 'training', f'Train_Dst_NoAuction_{method}_CF_{str(cf)}.txt')
     else:
         path = os.path.join(data_dir, method, 'testing', f'Test_Dst_NoAuction_{method}_CF_{str(cf)}.txt')
 
-    return np.loadtxt(str(path)).T
+    return np.loadtxt(str(path))
 
 
 def split_x_y(data):
-    """
-    X is input data for the models, Y is the label of 5 classification problems,
-    where 1 for up-movement, 2 for stationary condition, 3 is for down-movement
-    """
+    data = data.T
     data_length = 40
     x = data[:, :data_length]
     y = data[:, -5:]
@@ -28,37 +22,62 @@ def split_x_y(data):
 
 
 def group_ticks(x, y, T, k):
-    """
-    x_proc: 3D array that groups ticks into sequences of length T
-    y_proc: 1D array that selects the kth classification problem
-    """
     [N, D] = x.shape
 
-    # x processing
     x_proc = np.zeros((N - T + 1, T, D))
     for i in range(T, N + 1):
         x_proc[i - T] = x[i - T:i, :]
 
-    # y processing
     y_proc = y[T - 1:N]
     y_proc = y_proc[:, k] - 1
     return x_proc, y_proc
 
 
+def extract_stock(raw_data, stock_idx):
+    n_boundaries = 4
+    boundaries = np.sort(
+        np.argsort(np.abs(np.diff(raw_data[0], prepend=np.inf)))[-n_boundaries - 1:]
+    )
+    boundaries = np.append(boundaries, [raw_data.shape[1]])
+    split_data = tuple(raw_data[:, boundaries[i] : boundaries[i + 1]] for i in range(n_boundaries + 1))
+    return split_data[stock_idx]
+
+
+
 class FIDataset(Dataset):
+
     def __init__(self, data_dir, method='Zscore', cf=1, train=True, T=100, k=4):
         self.method = method
         self.cf = cf
-        self.train=True
+        self.train = train
+        self.data_dir = data_dir
+        self.T = T
+        self.k = k
 
-        x, y = split_x_y(get_raw_dataset(data_dir, method, cf, train))
-        x, y = group_ticks(x, y, T, k)
+        x, y = self.init_dataset()
 
         x = torch.from_numpy(x)
         self.x = torch.unsqueeze(x, 1)
         self.y = torch.from_numpy(y)
 
         self.length = len(y)
+
+    def init_dataset(self):
+        x_cat = np.array([])
+        y_cat = np.array([])
+        for stock in [0, 1, 2, 3, 4]:
+            day_data = extract_stock(
+                get_raw_dataset(self.data_dir, self.method, self.cf, self.train), stock)
+            x, y = split_x_y(day_data)
+            x_day, y_day = group_ticks(x, y, self.T, self.k)
+
+            if len(x_cat) == 0 and len(y_cat) == 0:
+                x_cat = x_day
+                y_cat = y_day
+            else:
+                x_cat = np.concatenate((x_cat, x_day), axis=0)
+                y_cat = np.concatenate((y_cat, y_day), axis=0)
+        return x_cat, y_cat
 
     def __len__(self):
         return self.length
